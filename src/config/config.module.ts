f755e715b476bbc491424c075bb061314d6624cd
B Bhargava chary <bhargavacharybasangari@gmail.com>
2025-03-14 22:44:53 +0530

    Implementing a Global Exception Filter in NestJS

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
+import { AllExceptionsFilter } from './allexceptions.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
     imports: [
@@ -9,6 +11,14 @@ import configuration from './configuration';
             envFilePath: '.env', //In production server use your desired env filename Ex.production.env in place of .env
             isGlobal: true, //If "true", the ConfigModule will registers as a global module in application. can be accessable in any module
         })
+    ],
+    providers: [
+        /* Global exception filter as provider
+        {
+            provide: APP_FILTER,
+            useClass: AllExceptionsFilter,
+        }
+        */
     ]
 })
 export class MyConfigModule { }
